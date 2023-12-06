e7582f531fbb8905569be7f89291f8348efd6132
Mike Storey <mike@ssgpp.com>
2023-12-05 20:55:06 -0500

    show the operation results until new number starts

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // theValue contains the currently calculated value
 let theValue = 0;
 
+// newValue is a flag to indicate that we are ready to start a new value
+let newValue = true;
+
 // saveOperator is the last operator that was used
 // this allows us to compute AFTER they have entered the next number
 let saveOperator = "";
@@ -27,25 +30,29 @@ function operate (operator) {
       break;
   } 
   saveOperator = operator;
-  document.getElementById('display').value = "";
-}
-
-
-// Function to update the display with a clicked button value
-function updateDisplay(value) {
-  displayValue = document.getElementById('display').value;
-  displayValue = displayValue + value;
-  document.getElementById('display').value = displayValue;
-}
-
-// Function to calculate the result
-function calculateResult() {
-  operate("");
+  newValue = true;
   document.getElementById('display').value = theValue;
 }
 
-// Function to clear the display
+
+// Function to update the display with a clicked button value. If we are starting a new number
+// then we should ignore the current value and start from an empty string, before appending 
+// the specified value to the display value.
+function updateDisplay(value) {
+  console.log(newValue);
+  let startingValue = document.getElementById('display').value;
+  if (newValue) {
+    startingValue = "";
+    newValue = false;
+  }
+  displayValue = startingValue + value;
+  document.getElementById('display').value = displayValue;
+}
+
+// Function to clear the display, and reset the gloabal variables
 function clearDisplay() {
+  newValue = true;
   theValue = 0;
+  saveOperator = "";
   document.getElementById('display').value = "";
 }
